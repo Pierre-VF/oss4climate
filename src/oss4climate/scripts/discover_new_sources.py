6fdcca030284d30fde1a74b163a8c5492de78639
Pierre V-F <pierre@pierrevf.consulting>
2024-10-28 15:56:25 +0000

    Adding a way to re-parse existing targets

--- src/oss4climate/scripts/discover_new_sources.py
+++ src/oss4climate/scripts/discover_new_sources.py
@@ -2,7 +2,20 @@
 This module contains methods to discover new sources of code
 """
 
-from oss4climate.scripts import FILE_INPUT_INDEX, ParsingTargets, log_info
+import os
+
+from tqdm import tqdm
+
+from oss4climate.scripts import (
+    FILE_INPUT_INDEX,
+    FILE_OUTPUT_LISTING_FEATHER,
+    log_info,
+)
+from oss4climate.src.nlp.search import SearchResults
+from oss4climate.src.parsers import (
+    ParsingTargets,
+    fetch_all_project_urls_from_markdown_str,
+)
 from oss4climate.src.parsers.github_data_io import extract_repository_organisation
 
 
@@ -16,7 +29,33 @@ def discover_repositories_in_existing_organisations(output_file: str) -> None:
     extended_targets = ParsingTargets(
         github_organisations=orgs,
     )
-    extended_targets.ensure_sorted_and_unique_elements()
 
-    # Exporting to TOML file
+    # Cleaning up and exporting to TOML file
+    log_info("Cleaning up targets")
+    extended_targets.cleanup()
+    log_info("Exporting to {output_file}")
     extended_targets.to_toml(output_file)
+
+
+def discover_repositories_in_existing_readmes(output_file: str) -> None:
+    log_info("Searching for relevant resource URLs in READMEs of known repositories")
+    if not os.path.exists(FILE_OUTPUT_LISTING_FEATHER):
+        raise RuntimeError(
+            "The dataset is not available locally - make sure to download it prior to running this"
+        )
+
+    dfs = SearchResults(FILE_OUTPUT_LISTING_FEATHER).documents
+
+    full_targets = ParsingTargets.from_toml(FILE_INPUT_INDEX)
+    for i, r in tqdm(dfs.iterrows()):
+        try:
+            if isinstance(r["readme"], str):
+                full_targets += fetch_all_project_urls_from_markdown_str(r["readme"])
+        except Exception as e:
+            print(f"Error with {r} // e={e}")
+
+    # Cleaning up and exporting to TOML file
+    log_info("Cleaning up targets")
+    full_targets.cleanup()
+    log_info("Exporting to {output_file}")
+    full_targets.to_toml(output_file)
