f0f1c16dd211f3878d4696d20a25df465b0b022b
Pierre V-F <pierre@pierrevf.consulting>
2024-12-18 14:21:57 +0000

    Marking test mode properly when running the app locally

--- app.py
+++ app.py
@@ -14,4 +14,7 @@ except ImportError:
 
 if __name__ == "__main__":
     # For local testing
+    from oss4climate_app import mark_test_mode
+
+    mark_test_mode()
     run(app, host="127.0.0.1", port=8080)
