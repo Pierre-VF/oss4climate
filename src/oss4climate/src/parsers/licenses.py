b02da538da7711a9480bb356f76b6ccaedfd7282
Pierre V-F <pierre@pierrevf.consulting>
2025-01-15 19:08:43 +0000

    Adding URLs of licenses

--- src/oss4climate/src/parsers/licenses.py
+++ src/oss4climate/src/parsers/licenses.py
@@ -20,6 +20,44 @@ class LicenseCategoriesEnum(Enum):
     UNKNOWN = "Unknown"
 
 
+# Using https://opensource.org/license/ to determine canonical URLs
+_url_by_license = {
+    "Apache License 2.0": "https://www.apache.org/licenses/LICENSE-2.0.txt",
+    'BSD 2-Clause "Simplified" License': "https://opensource.org/license/bsd-2-clause",  # Not ideal
+    "BSD 3-Clause Clear License": "https://opensource.org/license/bsd-3-clause",  # Not ideal
+    'BSD 3-Clause "New" or "Revised" License': "https://opensource.org/license/bsd-3-clause",  # Not ideal
+    "Creative Commons Attribution 4.0 International": "https://creativecommons.org/licenses/by/4.0/",
+    "Creative Commons Attribution Share Alike 4.0 International": "https://creativecommons.org/licenses/by-sa/4.0/",
+    "Creative Commons Attribution Non Commercial No Derivatives 4.0 International": "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    "Creative Commons Zero v1.0 Universal": "https://creativecommons.org/publicdomain/zero/1.0/",
+    "Eclipse Public License 1.0": "https://www.eclipse.org/org/documents/epl-1.0/EPL-1.0.txt",
+    "Eclipse Public License 2.0": "https://www.eclipse.org/legal/epl-2.0/",
+    "GNU Affero General Public License v3.0": "https://www.gnu.org/licenses/agpl-3.0.txt",
+    "GNU General Public License v2.0": "https://www.gnu.org/licenses/gpl-2.0.txt",
+    "GNU General Public License v3.0": "https://www.gnu.org/licenses/gpl-3.0.txt",
+    "GNU General Public License v3.0 only": "https://www.gnu.org/licenses/gpl-3.0.txt",
+    "GNU General Public License v3.0 or later": "https://www.gnu.org/licenses/gpl-3.0.txt",  # Not ideal
+    "GNU Lesser General Public License v2.1": "https://www.gnu.org/licenses/lgpl-2.1.txt",
+    "GNU Lesser General Public License v2.1 only": "https://www.gnu.org/licenses/lgpl-2.1.txt",
+    "GNU Lesser General Public License v3.0": "https://www.gnu.org/licenses/lgpl-3.0.txt",
+    "MIT License": "https://opensource.org/license/mit",  # Not ideal
+    "MIT No Attribution": "https://opensource.org/license/mit-0",  # Not ideal
+    "Academic Free License v3.0": "https://opensource.org/license/afl-3-0-php",  # Not ideal
+    "Artistic License 2.0": "https://www.perlfoundation.org/artistic-license-20.html",
+    "Boost Software License 1.0": "https://www.boost.org/LICENSE_1_0.txt",
+    "European Union Public License 1.1": "https://interoperable-europe.ec.europa.eu/licence/european-union-public-licence-version-11-eupl",
+    "European Union Public License 1.2": "https://interoperable-europe.ec.europa.eu/licence/european-union-public-licence-version-12-eupl",
+    "ISC License": "https://opensource.org/license/isc-license-txt",  # Not ideal
+    "Mozilla Public License 2.0": "https://www.mozilla.org/en-US/MPL/2.0/",
+    "Other": None,
+    "The Unlicense": "https://unlicense.org/",
+}
+
+
+def licence_url_from_license_name(name: str) -> str | None:
+    return _url_by_license.get(name)
+
+
 def license_category_from_license_name(name: str) -> LicenseCategoriesEnum:
     if not isinstance(name, str):
         out = LicenseCategoriesEnum.UNKNOWN
