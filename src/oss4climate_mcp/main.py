ae8ed0ff694b90494072a0c4b2af43b1777b12d5
Pierre V-F <pierre@pierrevf.consulting>
2026-03-14 19:17:21 +0100

    fix: basic MCP features

--- src/oss4climate_mcp/main.py
+++ src/oss4climate_mcp/main.py
@@ -3,12 +3,13 @@ Example of an MCP server using https://github.com/modelcontextprotocol/python-sd
 """
 
 import pandas as pd
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
+from fastmcp.exceptions import NotFoundError
 from oss4climate.src.config import SETTINGS
 from pydantic import BaseModel
 
 # Full indexing of the files
-df = pd.read_feather(
+df: pd.DataFrame = pd.read_feather(
     SETTINGS.get_listing_file_with_readme_and_description_file_columns()[0]
 )
 df["idx"] = df.index.to_series().astype(int)
@@ -16,40 +17,48 @@ df["idx"] = df.index.to_series().astype(int)
 # Create an MCP server
 mcp = FastMCP(
     "MCP for project data",
-    json_response=True,
+    # json_response=True,
     # stateless_http=True,
 )
 
 
 class ProjectDetails(BaseModel):
-    id: int
-    idx: int
+    idx: str
     name: str
     description: str
     language: str
     url: str
 
-
-@mcp.resource("resource://project/{url}")
-def read_project_details(url: str) -> ProjectDetails:
-    """Provides the details of a project for which the URL is provided.
-    This is meant to provide you context about projects from Github, Gitlab, Bitbucket and other Gitx places."""
-    return ProjectDetails(id=0, idx=0, name="", description="", language="", url="")
-
-
-@mcp.resource("resource://known-projects")
-def list_known_projects() -> list[ProjectDetails]:
-    """List all known projects"""
-    print("[list] LIST")
-    return []
+    @staticmethod
+    def from_row(r: dict | pd.Series) -> "ProjectDetails":
+        return ProjectDetails(
+            idx=str(r["id"]),
+            name=r["name"],
+            description=str(r["description"]),
+            language=str(r["language"]),
+            url=r["url"],
+        )
 
 
 @mcp.tool()
-def search_for_projects(topic: str) -> list[ProjectDetails]:
-    """Searches for projects that are related to a topic"""
-    print(f"[search] query = {topic}")
+def read_project_details(url: str) -> ProjectDetails:
+    """Provides the details of a project for which the URL is provided.
+    This is meant to provide you context about projects from Github, Gitlab, Bitbucket and other Gitx places.
+    """
+    r = df[df["url"] == url]
+    if len(r) < 1:
+        raise NotFoundError()
+    else:
+        return ProjectDetails.from_row(r.iloc[0])
 
-    return []
+
+@mcp.tool()
+def search_for_projects(topic: str, n_max_results: int = 50) -> list[ProjectDetails]:
+    """Searches for projects that are related to a topic.
+    `n_max_results` is an integer that sets the maximum number of results returned"""
+    print(f"[search] query = {topic}")
+    res = df[df["readme"].apply(lambda x: topic in x)].head(n_max_results)
+    return [ProjectDetails.from_row(i) for _, i in res.iterrows()]
 
 
 # Run with streamable HTTP transport
--- src/test/oss4climate_mcp/test_mcp.py
+++ src/test/oss4climate_mcp/test_mcp.py
@@ -1,13 +1,30 @@
 import pytest
 from fastmcp.client import Client
-from oss4climate_mcp.main import mcp
+from oss4climate_mcp.main import ProjectDetails, mcp
 
 
 @pytest.mark.asyncio
 async def test_list_tools():
-    async with Client(transport=mcp) as main_mcp_client:
-        list_tools = await main_mcp_client.list_tools()
-        list_resources = await main_mcp_client.list_resources()
+    async with Client(transport=mcp) as mcp_client:
+        list_tools = await mcp_client.list_tools()
+        list_resources = await mcp_client.list_resources()
 
         assert len(list_tools) > 0
-        assert len(list_resources) > 0
+        assert len(list_resources) >= 0
+
+        r = await mcp_client.call_tool(
+            "read_project_details", {"url": "https://github.com/Pierre-VF/oss4climate"}
+        )
+        # Check success of parsing
+        ProjectDetails(**r.data.__dict__)
+
+        n_max_results = 10
+        rs_raw = await mcp_client.call_tool(
+            "search_for_projects", {"topic": "solar", "n_max_results": n_max_results}
+        )
+        # Check success of parsing
+        rs = [ProjectDetails(**i.__dict__) for i in rs_raw.data]
+        assert isinstance(rs, list)
+        assert n_max_results >= len(rs) > 0
+        for i in rs:
+            assert isinstance(i, ProjectDetails)
